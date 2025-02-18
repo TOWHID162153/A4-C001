8a6a8ae56b3a33a6c9ad9d20f525661269c713f0
Towhid shahriar <web.development162253@gmail.com>
2025-02-18 14:01:04 +0600

    first commit

---
+++ problem-01.js
@@ -0,0 +1,19 @@
+function cashOut(money){
+    if(money >= 0 && money === Number(+ money)){
+        let chargeAmount = money * 1.75 / 100;
+        return chargeAmount;
+    }
+    else{
+        return "Invalid";
+    } 
+}
+
+cashOut(2000);
+cashOut(100);
+cashOut(999);
+cashOut(-350);
+cashOut(0);
+cashOut("101");
+cashOut("mewauu");
+
+
